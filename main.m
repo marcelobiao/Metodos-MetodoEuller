edbbd58f798bece408c6ea325db7a5df1cc93af8
marcelobiao <marcelobiao2@gmail.com>
2017-07-12 20:52:02 -0300

    Refatorando

--- main.m
+++ main.m
@@ -2,19 +2,18 @@ clc;
 %Definindo Parametros
 syms x y f;
 %Funcao:
-f=-y+x+2;     
+f=-1.2*y+7*exp(-0.3*x);     
 %Intervalo de x [a,b]:
 a=0;
-b=0.5;
+b=2.5;
 %Estimativa Inicial:
-y0=2;
+y0=3;
 %Numero de Pontos:
 n=6;
 
-[xEulerEx,yEulerIm]=metodoEulerExplicito(f,y0,a,b,n);
-%yHeun=metodoHeun(f,y0,a,b,n);
-%yPonto=metodoPontoCentral(f,y0,a,b,n);
-disp(xEulerEx);
-disp(yEulerIm);
-%disp(yHeun);
-%disp(yPonto);
+%[xEulerEx,yEulerIm]=metodoEulerExplicito(f,y0,a,b,n);
+%disp(xEulerEx);disp(yEulerIm);
+%[xHeun,yHeun]=metodoHeun(f,y0,a,b,n);
+%disp(xHeun);disp(yHeun);
+[xPontoM,yPontoM]=metodoPontoCentral(f,y0,a,b,n);
+disp(xPontoM);disp(yPontoM);

32ebc932718b5b0cf455f5749abf3267a308f592
marcelobiao <marcelobiao2@gmail.com>
2017-07-12 19:31:07 -0300

    Implementando Metodo de Euler Implicito

---
+++ metodoEulerImplicito.m
@@ -0,0 +1,16 @@
+%eq:Funcao.
+%y0:Estimativa Inicial.
+%[a,b]:Intervalo de x.
+%n:Numero de Pontos.
+
+function [xi,yi] = metodoEulerImplicito(eq,y0,a,b,n)
+	%Preparando parametros
+	h = (b - a)/(n-1);
+    syms x y f;
+    f=eq;
+	xi(1) = a;
+	yi(1) = y0;
+    
+	%Metodo
+	%TODO: Metodo resulta em uma equacao nao-linear. 
+end
